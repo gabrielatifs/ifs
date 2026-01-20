e5e5ca5249877821eeb77230f1b8b93cd4770dd8
Gabriel Brown <gabriel@cause.cx>
2026-01-20 03:16:08 +0000

    True Signout

--- apps/portal/src/components/utils/fastLogout.js
+++ apps/portal/src/components/utils/fastLogout.js
@@ -1,11 +1,6 @@
+import { fastLogout as sharedFastLogout } from '@ifs/shared/utils';
+
 export const fastLogout = () => {
     console.log('FASTLOGOUT CALLED');
-    alert('fastLogout called - clicking OK will redirect');
-
-    // Clear all auth-related storage
-    localStorage.clear();
-    sessionStorage.clear();
-
-    // Redirect immediately
-    window.location.href = 'https://www.join-ifs.org';
+    sharedFastLogout();
 };
